266476056021c97bd9f1b8f1f2cf91533add8e2c
kehinde <garbadeenk@gmail.com>
2022-03-10 14:31:31 -0800

    commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,13 @@
+
+#include <studio.h>
+
+/**
+ * main - prints the phrase "programming is like building a multilingual puzzle
+ * Return: 0 if exited properly, non-zero otherwise
+ * /
+ int main(void)
+ {
+      puts("\"programming is like building a multilingual puzzule");
+      return (0);
+
+ }
