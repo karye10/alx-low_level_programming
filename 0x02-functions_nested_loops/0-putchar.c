6b0592db9f8c3a2fcabe698e435cbfe847ad4856
kehinde <garbadeenk@gmail.com>
2022-03-15 13:20:37 -0700

    commit

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -1,9 +1,20 @@
-#include "holberton.h"
+#include "main.h"
 
 /**
- * main - prints Holberton, followed by a new line
+ * main - prints _putchar, followed by a new line
  * Return: Always 0 (Success)
  */
 int main(void)
 {
-	_putchar('H')
+	_putchar('_');
+	_putchar('p');
+	_putchar('u');
+	_putchar('t');
+	_putchar('c');
+	_putchar('h');
+	_putchar('a');
+	_putchar('r');
+	_putchar('\n');
+
+	return (0);
+}
